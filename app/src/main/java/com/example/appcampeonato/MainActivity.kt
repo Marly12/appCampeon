5e23cf23b062b0b4b3614d6478e045c16ef54920
Marly Yajaira Guerrero Florez <marlyg2@hotmail.com>
2021-10-18 17:20:21 -0500

    cuarta prueba

---
+++ app/src/main/java/com/example/appcampeonato/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.appcampeonato
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+    }
+}
