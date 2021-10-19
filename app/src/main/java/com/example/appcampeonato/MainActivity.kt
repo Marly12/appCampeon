9d55a647fdc881e8e30c7765d19df2c1729b3371
Marly Yajaira Guerrero Florez <marlyg2@hotmail.com>
2021-10-18 19:06:17 -0500

    cuarta prueba

--- app/src/main/java/com/example/appcampeonato/MainActivity.kt
+++ app/src/main/java/com/example/appcampeonato/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.appcampeonato
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        Thread.sleep(2000)
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        startTimer()
 
 
 
     }
+
+    fun startTimer(){
+        object: CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                val intent = Intent(applicationContext, MainActivity::class.java).apply {}
+                startActivity(intent)
+
+            }
+
+        }.start()
+    }
+
 }
